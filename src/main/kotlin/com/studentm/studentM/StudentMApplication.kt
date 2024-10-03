15e226ddf355eba223aa3da1252fb47b68a03021
Soohyun Lee <sb05037@gmail.com>
2024-10-03 15:28:17 +0900

    Git과 Github

---
+++ src/main/kotlin/com/studentm/studentM/StudentMApplication.kt
@@ -0,0 +1,11 @@
+package com.studentm.studentM
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentMApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentMApplication>(*args)
+}
---
+++ src/test/kotlin/com/studentm/studentM/StudentMApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.studentm.studentM
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentMApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
